c208352990a82515a8cedc062fe271f39026367e
podyankee <podyankee@gmail.com>
2021-04-05 06:50:19 +0300

    initial commit

---
+++ gulp/paths/css.vendor.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = [
+	'./node_modules/normalize.css/normalize.css',
+	// './node_modules/swiper/css/swiper.min.css',
+	// './node_modules/slick-carousel/slick/slick.css',
+	// './node_modules/magnific-popup/dist/magnific-popup.css'
+];
---
+++ readme.md
@@ -0,0 +1,1 @@
+eshop coding from "https://www.youtube.com/watch?v=Nxg6UfofZX0&t=30530s"
