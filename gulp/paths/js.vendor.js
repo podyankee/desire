8d21b876255040fd9e74e0215a1552021107711f
podyankee <podyankee@gmail.com>
2021-04-09 07:46:31 +0300

    top slider added

--- gulp/paths/js.vendor.js
+++ gulp/paths/js.vendor.js
@@ -3,6 +3,6 @@
 module.exports = [
 	// './node_modules/swiper/js/swiper.min.js',
 	'./node_modules/jquery/dist/jquery.min.js',
-	// './node_modules/slick-carousel/slick/slick.min.js',
+	'./node_modules/slick-carousel/slick/slick.min.js',
 	// './node_modules/magnific-popup/dist/jquery.magnific-popup.min.js'
 ];
